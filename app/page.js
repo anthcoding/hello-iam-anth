eb6f1b68e3979f36edbb5e40ddbe62c4d7357827
Anth Lorenzo <anthlorenzo@Anths-Air.attlocal.net>
2023-09-09 22:59:55 -0400

    Adding basic info to do deployment

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
 	return (
 		<main className="flex items-center justify-center h-[100Vh] w-full">
-			<p>In development at this point...</p>
+			<p>This website is under construction 🚧 </p>
 		</main>
 	);
 }
